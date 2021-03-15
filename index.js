001cfccf420598bc55335b39b092f4494648fe4e
priyam1103 <priyampoddar89@gmail.com>
2021-03-15 22:43:15 +0530

    production changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3005;
 const app = express();
 const config = require("./service/config");
 const { connectDb } = require("./service/db");
-
+process.env.NODE_TLS_REJECT_UNAUTHORIZED='0'
 app.use(cors());
 require("./service/routes")(app);
 
@@ -15,6 +15,6 @@ connectDb().then(() => {
   app.listen(config.PORT, () => {
     console.log(`Connected to port ${config.PORT}`);
   });
-});
+}).catch((err)=>{});
 
 
