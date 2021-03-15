60d7628185eaa332f2df49bce7d96230b1917d8f
priyam1103 <priyampoddar89@gmail.com>
2021-03-15 22:45:19 +0530

    reverse change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3005;
 const app = express();
 const config = require("./service/config");
 const { connectDb } = require("./service/db");
-process.env.NODE_TLS_REJECT_UNAUTHORIZED='0'
+
 app.use(cors());
 require("./service/routes")(app);
 
